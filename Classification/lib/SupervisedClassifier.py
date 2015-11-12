958ad907431235db5102321c0857a54be03e9bae
Damian Polan <damian.polan@gmail.com>
2015-11-12 16:49:46 -0500

    Added SupervisedClassifier abstract class

---
+++ Classification/lib/SupervisedClassifier.py
@@ -0,0 +1,29 @@
+class SupervisedClassifier:
+    """
+    Classifies a single pack of data.
+
+    """
+
+
+    def __init__(self):
+        raise "Not implemented"
+    
+
+    def fit(self, data, classification):
+        """
+        Supervised training fitting the given feature data to the target.
+
+        @param data: The list of training vectors to fit.
+        @param classification: Target classifications of the training vectors.
+
+        """
+        raise "Not implemented"
+
+
+    def classify(self, item):
+        raise "Not implemented"
+
+
+
+
+
