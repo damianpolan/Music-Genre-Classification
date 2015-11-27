15339875aa1f43753b801b61a03213341affd7c4
Damian Polan <damian.polan@gmail.com>
2015-11-27 17:53:06 -0500

    Implemented logging

--- lib/classification/test.py
+++ lib/classification/test.py
@@ -2,17 +2,31 @@
 
 import sys
 import os
+import logging
   
 sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
 
 from algorithms import SupportVectorCLS 
-from fingerprints import DatabaseController
+from fingerprints import database
+from fingerprints import tools
 
 
 def main(argv):
+    tools.defaultLog()
+
+    packsize = 512
 
 
-    #classifier = SupportVectorCLS()
+    dbControl = database.Controller()
+
+    dbControl.getTrainingSet(['trance', 'dubstep'], -1)
+
+
+
+    classifier = SupportVectorCLS()
+
+    #assign descriptors and classifications
+
 
     pass
 
---
+++ lib/classification/validation.py
@@ -0,0 +1,7 @@
+
+
+
+class EightFoldValidation():
+    "Permorms an 8 fold validation technique"
+    def __init__(self):
+        pass
